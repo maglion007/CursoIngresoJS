84925ad2ff57ac3cf4b3426e672d0d1f9835da8b
maglion007 <36174199+maglion007@users.noreply.github.com>
2018-02-17 12:10:36 -0300

    ejercicio 1,2 y 3

--- 3-InstruccionSwitch/jsInstruccionSwitch(2).js
+++ 3-InstruccionSwitch/jsInstruccionSwitch(2).js
@@ -5,6 +5,26 @@ var mesDelAño = document.getElementById('mes').value;
 
 alert (mesDelAño);
 
+  switch(mesDelAño)
+  {
+  	case "Julio"  :
+  	case "Agosto"  :
+  	alert("estamos en invierno");
+  	break;
+  	case "Enero"  :
+  	case "Febrero"  :
+  	case "Abril"  :
+  	case "Mayo"  :
+  	case "Junio"  :
+  	alert ("falta para el invierno");
+  	break;
+  	case "Septiembre"  :
+  	case "Octubre"  :
+  	case "Noviembre"  :
+  	case "Diciembre"  :
+  	alert("ya pasamos el frio, ahora calor!!!");
+  	break;
+   }
 
 
 
