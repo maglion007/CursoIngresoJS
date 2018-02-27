45323e60d58aa644bcd868792bbdcb6e8ec21a0a
maglion007 <36174199+maglion007@users.noreply.github.com>
2018-02-26 21:24:07 -0300

    uno, dos y tres

--- 9-Parciales/parcial/dos.js
+++ 9-Parciales/parcial/dos.js
@@ -1,4 +1,17 @@
 function Mostrar()
+
+
+//precio y porcentaje de descuento
+
 {
+   var importe;
+   var importeFinal;
+   
+   importe=prompt("ingresar importe");
+   importeFinal=importe*1.21;
+   document.getElementById('importeFinal').value=importeFinal;
+
+   
   
+
 }
--- 9-Parciales/parcial/uno.js
+++ 9-Parciales/parcial/uno.js
@@ -2,4 +2,19 @@
 function Mostrar()
 {
 
+	var base;
+	var altura;
+	var perimetro;
+	var superficie;
+
+	base=document.getElementById('laBase').value;
+	altura=prompt("ingresar altura ");
+
+	perimetro=base*3;
+	superficie=(base*altura)/2;
+
+	alert("el perimetro es " + perimetro + "y la superficie es " + superficie );
+
+
+
 }
