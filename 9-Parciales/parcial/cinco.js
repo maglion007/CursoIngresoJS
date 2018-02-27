36fe5afd48b150edbf7ec9b849d8c68733abed9d
maglion007 <36174199+maglion007@users.noreply.github.com>
2018-02-27 16:34:25 -0300

    ejercicio 5

--- 9-Parciales/parcial/cinco.js
+++ 9-Parciales/parcial/cinco.js
@@ -1,4 +1,25 @@
 function Mostrar()
 {
+	var diaDeSemana;
+
+	diaDeSemana=prompt("ingrese un dia");
+
+	switch(diaDeSemana)
+
+	{
+
+		case "Sabado":
+		case "Domingo":
+		alert("es fin de semana");
+		break;
+
+		case "Lunes":
+		case "Martes":
+		case "Miercoles":
+		case "Jueves":
+		case "Viernes":
+		alert("a trabajar!!!");
+		break;
+	}
 
 }
