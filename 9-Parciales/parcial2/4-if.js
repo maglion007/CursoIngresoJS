95fa0338cf931591f556d2257c271ad9a4fceea2
maglion007 <36174199+maglion007@users.noreply.github.com>
2018-02-27 22:08:35 -0300

    Add files via upload

--- 9-Parciales/parcial2/4-if.js
+++ 9-Parciales/parcial2/4-if.js
@@ -1,7 +1,11 @@
 //Debemos lograr mostrar un mensaje al presionar el botón  'MOSTRAR'.
 function Mostrar()
 {
-	alert("Funciona 4-if");
+  
 	
 }
 
+//se ingresan dos num, si son iguales se concatenan,
+// si el primero es mayor que el segundo se multiplican, 
+//si el segundo es menor al grande se restan, 
+//si la * da un num par ademas de mostrar el num del resultado se muestra el msj spart
--- 9-Parciales/parcial2/8-iteraciones.js
+++ 9-Parciales/parcial2/8-iteraciones.js
@@ -5,3 +5,6 @@ function Mostrar()
 	
 }
 
+//el nombre de un animal, el pseo de un animal y la temperatura del habitat en que vive, entre -40 y +40
+//al final debemos informar la cant de temp pares ingresadas, la cant de temp impares, el nombre del animal mas pesado y menos pesado,la cant de animales cuyo habitat tiene -0 grados
+//el promedio de peso de todos los animales, temp max y min
